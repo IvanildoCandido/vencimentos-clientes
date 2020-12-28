c4d467e620301e3d896962fafc41ddb978a009db
Ivanildo Cândido <systechtecnologia@gmail.com>
2020-12-28 19:48:06 -0300

    Inicio : Sistema Controle de Vencimentos

---
+++ script.js
@@ -0,0 +1,36 @@
+const txtName = document.querySelector('#txtName');
+const txtValue = document.querySelector('#txtValue');
+const txtDueDate = document.querySelector('#txtDueDate');
+const clientList = document.querySelector('.clientList');
+const btnAdd = document.querySelector('#btnAdd');
+let data = [];
+btnAdd.addEventListener('click', addToList);
+
+function addToList() {
+  const dataObject = {};
+  dataObject.name = txtName.value;
+  dataObject.dueDate = txtDueDate.value;
+  dataObject.amount = txtValue.value;
+  createRow(dataObject);
+  saveClient(dataObject);
+}
+function createRow(data) {
+  const entriesClient = Object.entries(data);
+  const tr = document.createElement('tr');
+  entriesClient.forEach(([key, value]) => {
+    const td = document.createElement('td');
+    td.innerText = data[key];
+    tr.appendChild(td);
+  });
+  clientList.appendChild(tr);
+}
+async function saveClient(client) {
+  data.push(client);
+  localStorage.setItem('clients', JSON.stringify(data));
+}
+window.onload = function () {
+  function getData() {
+    data = JSON.parse(localStorage.getItem('clients'));
+  }
+  getData();
+};
